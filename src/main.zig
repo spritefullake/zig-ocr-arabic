787b4f2f1df168db7787d4de9047387dd5ff6862
Talha Ahmad <gamezen@icloud.com>
2023-08-08 18:15:48 -0400

    removed types; easier c integration

--- src/main.zig
+++ src/main.zig
@@ -13,63 +13,13 @@ const magick_wand = @cImport({
 const ImageMagick = @cImport({
     @cInclude("MagickCore/MagickCore.h");
 });
-const LeptonicaErrors = error{FileNotRead};
-const Box = extern struct {};
-const PixColormap = extern struct {};
-const Pix = extern struct { w: c_ulong, h: c_ulong, d: c_ulong, wpl: c_ulong, xres: c_long, yres: c_long, informat: c_long, text: [*c]const u8, colormap: *PixColormap, data: *c_ulong };
-const Pixa = extern struct { n: c_long, nalloc: c_long, refcount: c_ulong, pix: [*]*Pix, boxa: *Box };
 
-const TessBaseAPI = struct {}; //@TypeOf(tesseract.TessBaseAPICreate());
-const LeptPixaPtr = leptonica.pixaReadMultipageTiff("example");
-const Renderer = extern struct {};
-extern fn pixaReadMultipageTiff(image_path: [*c]const u8) [*c]Pixa;
-extern fn pixReadTiff(image_path: [*c]const u8, n: c_long) [*c]Pix;
-extern fn pixRead(image_path: [*c]const u8) *Pix;
-extern fn TessBaseAPISetImage2(api: [*c]TessBaseAPI, [*c]Pix) void;
-extern fn TessBaseAPIProcessPage(handle: [*c]TessBaseAPI, pix: [*c]Pix, page_index: c_long, filename: [*c]const u8, retry_config: [*c]const u8, timeout_millisec: c_long, renderer: [*c]Renderer) c_uint;
-extern fn TessBaseAPIProcessPages(handle: [*c]TessBaseAPI, filename: [*c]const u8, retry_config: [*c]const u8, timeout_millisec: c_long, renderer: [*c]Renderer) c_short;
-extern fn TessBaseAPIGetUTF8Text(api: [*c]TessBaseAPI) [*c]const u8;
-extern fn TessPDFRendererCreate(outputbase: [*c]const u8, datadir: [*c]const u8, text_only: c_short) [*c]Renderer;
-extern fn TessAltoRendererCreate(outputbase: [*c]const u8) [*c]Renderer;
-extern fn TessResultRendererAddImage(renderer: [*c]Renderer, api: [*c]TessBaseAPI) c_short;
-extern fn TessBaseAPICreate() [*c]TessBaseAPI;
-extern fn TessBaseAPIInit3(handle: [*c]TessBaseAPI, datapath: [*c]const u8, language: [*c]const u8) c_short;
-extern fn TessBaseAPIReadConfigFile(handle: [*c]TessBaseAPI, config_file: [*c]const u8) void;
-extern fn TessBaseAPIDelete(handle: [*c]TessBaseAPI) void;
-extern fn TessBaseAPIEnd(handle: [*c]TessBaseAPI) void;
-extern fn TessResultRendererEndDocument(renderer: [*c]Renderer) c_short;
-extern fn TessDeleteResultRenderer(renderer: [*c]Renderer) void;
-fn mypixaReadMultipageTiff(allocator: std.mem.Allocator, image_path: []const u8) !*Pixa {
-    const result: *Pixa = pixaReadMultipageTiff(@ptrCast(image_path));
-    const pixa_ptr: *Pixa = try allocator.create(Pixa);
-    //defer allocator.destroy(pixa_ptr);
-    pixa_ptr.* = result.*;
-    const nalloc: usize = @intCast(result.nalloc);
-    const n: usize = @intCast(result.n);
-    const pixes_array_ptr = try allocator.alloc(*Pix, nalloc);
-    _ = pixes_array_ptr;
-
-    var i: usize = 0;
-    while (i < n) {
-        std.debug.print("The pixel at {} is {} width \n", .{ i, result.pix[i].w });
-        pixa_ptr.pix[i] = result.pix[i];
-        i += 1;
-    }
-
-    return pixa_ptr;
-}
 fn pdfToImageIntermediate(input_pdf: [*c]const u8, output_image: [*c]const u8) void {
     var mw: ?*magick_wand.MagickWand = null;
     magick_wand.MagickWandGenesis();
     defer magick_wand.MagickWandTerminus();
     mw = magick_wand.NewMagickWand();
-
-    var i: i32 = 0;
-    _ = magick_wand.MagickSetResolution(mw.?, 300, 300);
     _ = magick_wand.MagickReadImage(mw.?, input_pdf);
-
-    var color = magick_wand.NewPixelWand();
-    _ = magick_wand.PixelSetColor(color.?, "white");
     //very important to use the .MagickSet methods WITHOUT putting the word Image in between them
     // by ONLY using .MagickSet, we can apply global settings
     // Turns out .tiff file compression and downscaling is very important to the functioning of tesseract
@@ -77,42 +27,35 @@ fn pdfToImageIntermediate(input_pdf: [*c]const u8, output_image: [*c]const u8) v
     _ = magick_wand.MagickSetCompressionQuality(mw, 50);
     _ = magick_wand.MagickSetDepth(mw, 8);
     _ = compressionStatus;
-    while (i < magick_wand.MagickGetNumberImages(mw)) {
-        i += 1;
-        _ = magick_wand.MagickSetIteratorIndex(mw, i);
-        _ = magick_wand.MagickSetImageAlphaChannel(mw, magick_wand.RemoveAlphaChannel);
-        _ = magick_wand.MagickSetImageBackgroundColor(mw, color);
-    }
-    _ = magick_wand.MagickResetIterator(mw);
     _ = magick_wand.MagickSetFormat(mw, "png");
     _ = magick_wand.MagickWriteImages(mw, output_image, magick_wand.MagickTrue);
-
     defer {
         _ = magick_wand.DestroyMagickWand(mw);
-        _ = magick_wand.DestroyPixelWand(color.?);
     }
 }
-fn imageIntermediateToPdf(input_image: [*c]const u8, output_pdf_path: [*c]const u8) void {
+fn setUpTesseractAPI(allocator: std.mem.Allocator, tessdata_path: [*c]const u8, config_file: [*c]const u8) !*@TypeOf(tesseract.TessBaseAPICreate()) {
     //setup the tesseract api handle
-    const api = TessBaseAPICreate();
-    const tessdata_path = "./deps/tesseract/tessdata";
-    _ = TessBaseAPIInit3(api, tessdata_path, "ara");
-    TessBaseAPIReadConfigFile(api, "./pdf_config.txt");
-    defer {
-        TessBaseAPIDelete(api);
-        TessBaseAPIEnd(api);
-    }
+    const api = try allocator.create(@TypeOf(tesseract.TessBaseAPICreate()));
+    api.* = tesseract.TessBaseAPICreate();
 
+    _ = tesseract.TessBaseAPIInit3(api.*, tessdata_path, "ara");
+    tesseract.TessBaseAPIReadConfigFile(api.*, config_file);
+
+    return api;
+}
+fn imageIntermediateToPdf(api: *@TypeOf(tesseract.TessBaseAPICreate()), tessdata_path: [*c]const u8, input_image: [*c]const u8, output_pdf_path: [*c]const u8) void {
     const timeout_ms: c_int = 20000;
     const retry_config: ?*const u8 = null;
 
     const output_path = output_pdf_path;
     const text_only = 0; //aka false
 
-    const renderer = TessPDFRendererCreate(output_path, tessdata_path, text_only); //zero is important so we make the text appear visible
-    _ = TessBaseAPIProcessPages(api, input_image, retry_config, timeout_ms, renderer);
+    const renderer = tesseract.TessPDFRendererCreate(output_path, tessdata_path, text_only); //zero is important so we make the text appear visible
+    _ = tesseract.TessBaseAPIProcessPages(api.*, input_image, retry_config, timeout_ms, renderer);
 }
 pub fn main() !void {
+    const config_file = "./pdf_config.txt";
+    const tessdata_path = "./deps/tesseract/tessdata";
 
     //setup printing
     const stdout_file = std.io.getStdOut().writer();
@@ -121,9 +64,14 @@ pub fn main() !void {
     _ = stdout;
     //create the allocator
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
     defer arena.deinit();
     const allocator = arena.allocator();
+
+    const api = try setUpTesseractAPI(allocator, tessdata_path, config_file);
+    defer {
+        tesseract.TessBaseAPIDelete(api.*);
+        tesseract.TessBaseAPIEnd(api.*);
+    }
 
     //start timing
     const timer = try allocator.create(time.Timer);
@@ -131,8 +79,9 @@ pub fn main() !void {
 
     const input_pdf = "./test_pdf_arabic_short.pdf";
     const intermediate_image = "./data-out/second.tiff";
+    const output_pdf_path = "./data-out/final";
     pdfToImageIntermediate(input_pdf, intermediate_image);
-    imageIntermediateToPdf(intermediate_image, "./data-out/output");
+    imageIntermediateToPdf(api, tessdata_path, intermediate_image, output_pdf_path);
 
     const elapsed = @as(f64, @floatFromInt(timer.read()));
 
